8db87484077867fb29ff412765ce6cd407f43f29
willymusyoka <willykyeni@gmail.com>
2025-08-19 15:48:34 +0300

    Completes the SQL queries for managing bear data in a database, including creating a table, inserting records, and querying for specific information about bears.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'Brown', 'Calm', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 21, 'M', 'White', 'Calm', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 22, 'F', 'Black', 'Playful', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 23, 'M', 'Brown', 'Friendly', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 24, 'F', 'Brown', 'Aggressive', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 25, 'M', 'Cinnamon', 'Curious', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 26, 'F', 'Black', 'Friendly', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 27, 'F', 'White', 'Shy', TRUE);
