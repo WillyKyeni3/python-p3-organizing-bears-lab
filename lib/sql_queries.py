a2be80b19ad7f15e3cb460f6ce97dc244732d945
Willy Kyeni <133875723+WillyKyeni3@users.noreply.github.com>
2025-08-20 08:50:37 +0300

    Update sql_queries.py

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,39 +1,34 @@
 select_all_female_bears_return_name_and_age = """
-    SELECT 
-        bears.name, 
-        bears.age 
-    FROM bears 
-    WHERE sex = 'F';
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE sex='F'
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    SELECT 
-        bears.name 
-    FROM bears 
-    ORDER BY bears.name ASC;
+    SELECT name
+    FROM bears
+    ORDER BY name ASC;
+        
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    SELECT 
-        bears.name, 
-        bears.age 
-    FROM bears 
-    WHERE bears.alive = TRUE 
-    ORDER BY bears.age ASC;
+    SELECT name, age 
+    FROM bears
+    WHERE alive = 1
+    ORDER BY age ASC;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    SELECT 
-        bears.name, 
-        bears.age 
-    FROM bears 
-    WHERE bears.age = (SELECT MAX(age) FROM bears)
-      AND bears.name IS NOT NULL;
+    SELECT name, age
+    FROM bears
+    ORDER BY age DESC 
+    LIMIT 1;
 """
 select_youngest_bear_and_returns_name_and_age = """
-    SELECT 
-        bears.name, 
-        bears.age 
-    FROM bears 
-    WHERE bears.age = (SELECT MIN(age) FROM bears);
+    SELECT name, age
+    FROM bears
+    ORDER BY age ASC
+    LIMIT 1
 """
