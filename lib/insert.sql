bc80ef8e055aab8ed18415970e9f7096b8c645a3
Willy Kyeni <133875723+WillyKyeni3@users.noreply.github.com>
2025-08-20 08:46:38 +0300

    Update insert.sql

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr Chocolate', 20, 'M', 'Brown', 'Calm', 1);
 INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 21, 'M', 'White', 'Calm', 1);
 INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 22, 'F', 'Black', 'Playful', 1);
 INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 23, 'M', 'Brown', 'Friendly', 1);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 24, 'F', 'Brown', 'Aggressive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 24, 'F', 'Brown', 'Aggressive', 0);
 INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 25, 'M', 'Cinnamon', 'Curious', 1);
 INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 26, 'F', 'Black', 'Friendly', 1);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 27, 'F', 'White', 'Shy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 27, 'F', 'White', 'Shy', 0);
